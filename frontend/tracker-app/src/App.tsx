0012cf0624a040b514edf321c50ca8f9a52003d0
chany0ng <krap5075@naver.com>
2024-02-28 13:08:41 +0900

    component setting

--- frontend/tracker-app/src/App.tsx
+++ frontend/tracker-app/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
-
+import Greeter from "./components/Greeter";
+import ShoppingList from "./components/ShoppingList";
 function App() {
+  const items = [
+    { id: 1, product: "Lemon", quantity: 3 },
+    { id: 2, product: "Apple", quantity: 2 },
+  ];
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <ShoppingList items={items} />
     </div>
   );
 }
