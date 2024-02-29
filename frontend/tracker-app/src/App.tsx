ff54c19df466f4f663f499737080ef15e1e97fae
chany0ng <krap5075@naver.com>
2024-03-01 03:29:35 +0900

    config: install uuid

--- frontend/tracker-app/src/App.tsx
+++ frontend/tracker-app/src/App.tsx
@@ -3,13 +3,17 @@ import "./App.css";
 import Greeter from "./components/Greeter";
 import ShoppingList from "./components/ShoppingList";
 import Item from "./components/item";
-
+import ShoppingListForm from "./components/ShoppingListForm";
+import { v4 as uuid4 } from "uuid";
 function App() {
   const [items, setItems] = useState<Item[]>([]);
-
+  const AddItem = (product: string) => {
+    setItems([...items, { id: uuid4(), product: "abc", quantity: 1 }]);
+  };
   return (
     <div className="App">
       <ShoppingList items={items} />
+      <ShoppingListForm onAddItem={AddItem} />
       <a href="http://localhost:8080/login/oauth2/code/google">Google Login</a>
       <br />
       <a href="http://localhost:8080/login/oauth2/code/naver">Naver Login</a>
