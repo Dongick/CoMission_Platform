43edd0bf2a2a28f7e700ac54badeebf6bf311ffd
ChanYong Park <krap5075@naver.com>
2024-03-04 22:47:09 +0900

    feat: 로그인페이지추가

---
+++ frontend/tracker-app/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+const LoginPage = () => {
+  return (
+    <>
+      <h1>This is LoginPage</h1>
+      <a href="http://localhost:8080/login/oauth2/code/google">Google Login</a>
+      <br />
+      <a href="http://localhost:8080/login/oauth2/code/naver">Naver Login</a>
+    </>
+  );
+};
+
+export default LoginPage;
