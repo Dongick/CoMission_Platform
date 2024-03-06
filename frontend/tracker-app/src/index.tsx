c27baaf62171b38d5efccb63a2c50e70585357e3
ChanYong Park <krap5075@naver.com>
2024-03-06 17:31:00 +0900

    feat: queryClient 정의

--- frontend/tracker-app/src/index.tsx
+++ frontend/tracker-app/src/index.tsx
@@ -8,7 +8,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // react-qeury Client instance 생성
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+      retry: 1,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
