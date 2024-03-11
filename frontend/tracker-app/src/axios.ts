ed463ec8328e4f49f28396f4fb0f017ee024d218
ChanYong Park <krap5075@naver.com>
2024-03-11 19:15:47 +0900

    feat: axios 변경(AxiosResponse 이용)

--- frontend/tracker-app/src/axios.ts
+++ frontend/tracker-app/src/axios.ts
@@ -1,4 +1,4 @@
-import axios, { Axios, AxiosRequestConfig } from "axios";
+import axios, { Axios, AxiosRequestConfig, AxiosResponse } from "axios";
 import { APIResponse } from "./interfaces";
 
 // Axios instance 생성
@@ -10,10 +10,15 @@ const client: Axios = axios.create({
   //  image나 영상 같은 거를 formData로 보낼 때에 는 multipart/form-data를 메서드 정의할 때 header에 작성
 });
 
+//todo intercepter 추가 해야되는지 고민
+
 // GET Method
-export const getData = async <T>(url: string): Promise<APIResponse<T>> => {
+export const getData = async <T>(
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<T> => {
   try {
-    const response = await client.get<APIResponse<T>>(url);
+    const response = await client.get<T>(url, config);
     return response.data;
   } catch (error) {
     throw error;
@@ -21,3 +26,15 @@ export const getData = async <T>(url: string): Promise<APIResponse<T>> => {
 };
 
 // POST Method
+export const postData = async <T>(
+  url: string,
+  data: T,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  try {
+    const response = await client.post<T>(url, data, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
