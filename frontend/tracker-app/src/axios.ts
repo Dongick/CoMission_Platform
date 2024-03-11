86ca1b7720725b4a09511ff78bb81f34184009cb
ChanYong Park <krap5075@naver.com>
2024-03-11 19:51:57 +0900

    feat: axios interceptor setting

--- frontend/tracker-app/src/axios.ts
+++ frontend/tracker-app/src/axios.ts
@@ -1,8 +1,13 @@
-import axios, { Axios, AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, {
+  Axios,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
 import { APIResponse } from "./interfaces";
 
 // Axios instance 생성
-const client: Axios = axios.create({
+const instance: Axios = axios.create({
   baseURL: "http://localhost:3000", // BASE URL
   headers: {
     "Content-Type": "application/json",
@@ -10,7 +15,36 @@ const client: Axios = axios.create({
   //  image나 영상 같은 거를 formData로 보낼 때에 는 multipart/form-data를 메서드 정의할 때 header에 작성
 });
 
-//todo intercepter 추가 해야되는지 고민
+// 요청 interceptor
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+// 응답 interceptor
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    //todo 응답 시 콜백함수 추가 가능
+    return response;
+  },
+  (error) => {
+    // 토큰 만료 확인
+    if (error.response && error.response.status === 401) {
+      // 여기서 토큰 갱신 등의 작업 수행 가능
+      // 예: refreshToken을 사용하여 새로운 accessToken을 요청
+    }
+    return Promise.reject(error);
+  }
+);
 
 // GET Method
 export const getData = async <T>(
@@ -18,7 +52,7 @@ export const getData = async <T>(
   config?: AxiosRequestConfig
 ): Promise<T> => {
   try {
-    const response = await client.get<T>(url, config);
+    const response = await instance.get<T>(url, config);
     return response.data;
   } catch (error) {
     throw error;
@@ -32,7 +66,7 @@ export const postData = async <T>(
   config?: AxiosRequestConfig
 ): Promise<T> => {
   try {
-    const response = await client.post<T>(url, data, config);
+    const response = await instance.post<T>(url, data, config);
     return response.data;
   } catch (error) {
     throw error;
